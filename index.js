b7b32cc151a4d6b467f5653d438788f9577d390b
awwwagency <awwwagency@gmail.com>
2021-11-16 21:57:43 -0800

    wip

--- index.js
+++ index.js
@@ -16,19 +16,63 @@ window.onload = function(){
 
 let burgerMenu = document.querySelector('.menu');
 let navigation = document.querySelector('.nav');
+let page = document.querySelector('html');
 
 burgerMenu.addEventListener('click', () => {
+    page.classList.toggle("no-scroll");
 	burgerMenu.classList.toggle('active');
     navigation.classList.toggle("visible");
 });
 
 myTimer();
 
-var myVar = setInterval(function() {
+let myVar = setInterval(function() {
     myTimer();
   }, 1000);
   
   function myTimer() {
-    var d = new Date();
-    document.getElementById("local-time").innerHTML = d.toLocaleTimeString();
+    let d = new Date();
+
+    let hours = d.getHours();
+    let minutes = d.getMinutes();
+    let suffix = hours >= 12 ? 'PM' : 'AM';
+    let northAmericanHours = hours == 0 ? 12 : (hours <= 12 ? hours : hours - 12);
+    let time = northAmericanHours + ":" + minutes + " " + suffix;
+
+    document.getElementById("local-time").innerHTML = time;
   }
+
+// scroll 
+  $(function() {
+var $txtSliderUl = $('.text-slider ul'),
+    $txtSliderli = $txtSliderUl.append($txtSliderUl.html()).children(),
+    _height = $('.text-slider').height() * -1,
+    scrollSpeed = 600,
+    timer,
+    speed = 1000 + scrollSpeed;
+
+function sliderText() {
+var _now = $txtSliderUl.position().top / _height;
+_now = (_now + 1) % $txtSliderli.length;
+
+$txtSliderUl.animate({
+  top: _now * _height
+}, scrollSpeed, function() {
+
+  if (_now == $txtSliderli.length / 2) {
+    $txtSliderUl.css('top', 0);
+  }
+
+});
+
+timer = setTimeout(sliderText, speed);
+}
+
+$txtSliderli.hover(function() {
+clearTimeout(timer);
+}, function() {
+timer = setTimeout(sliderText, speed);
+});
+
+timer = setTimeout(sliderText, speed);
+});
